fa40b326320ef6d1cc2c7423c0be5a00e1c6490c
sohilax <sohila.mohammedx@gmail.com>
2021-09-21 19:22:50 +0200

    hola

---
+++ js/main.js
@@ -0,0 +1,176 @@
+// var userName = 20;
+// var role = "mustafa";
+
+
+// if (role == "mustafa") {
+//     console.log(" Your File Admin");
+// } else if (role == "ndjxc") {
+//     console.log("jsbdjasbdhab")
+// } else { window.alert("hahahahahaha") }
+
+
+// var nmp = prompt("enter");
+// for (var i = 0; i < nmp; i++) {
+//     if (i % 2 != 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// var src = prompt("Your Name");
+
+// function nams(name) {
+//     document.getElementById("title").innerHTML = "welcome " + name;
+// }
+
+// nams(src)
+
+// var user = 10;
+// if (user < 10) {
+
+//     console.log("welcome you can regster")
+// } else if (user >= 10) {
+
+//     alert("hello client")
+// }
+
+// var message = "hello adminstion";
+// var mana = "hello to Manager "
+// var role = prompt(" Your Role");
+
+// if (role == "admin") {
+//     console.log(message);
+// } else if (role == "manager") {
+//     window.alert("go to hele")
+// } else if (role == "prog") {
+
+//     document.getElementById("dome").innerHTML = "hello WebSite To Function";
+
+//     // document.getElementById("dome").innerHTML = "welcome ";
+
+// } else {
+//     document.getElementById("cant").innerHTML = mana
+// }
+
+
+// var userNamber = window.prompt("Your to a Namber");
+
+// for (var i = 0; i < userNamber; i + 25) {
+//     if (i <= 100) {
+//         console.log("hello")
+//     }
+// }
+
+
+
+
+
+// for (var i = 0; i < 10; i += 2) {
+//     // if (i % 2 == 0) {
+//     console.log("i");
+//     // }
+// }
+// var namber = window.prompt("hello Msut");
+
+// for (var i = 0; i < namber; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i)
+//     }
+// }
+
+// var laptop = "";
+// for (var i = 0; i <= 1; i++) {
+//     laptop += ` <div class="col-md-4 col-sm-12  ">
+//     <div class=""> <img class="w-100 " src="images/team-2.jpg" alt="team">
+
+//                     <div class="caption bg-info">
+
+//                         <h2>Lorem, ipsum dolor.</h2>
+//                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, ea!</p>
+//                     </div>
+d
+
+//             </div>
+
+//     </div>
+// `;
+
+// }
+
+
+
+// document.getElementById('demo').innerHTML = laptop
+
+
+var headSection = "";
+
+// for (var i = 0; i <= 1; i++) {
+//     headSection += `
+//     <nav id="navbar-example2" class="navbar navbar-light bg-light px-3">
+//   <a class="navbar-brand" href="#">Navbar</a>
+//   <ul class="nav nav-pills">
+//     <li class="nav-item">
+//       <a class="nav-link" href="#scrollspyHeading1">First</a>
+//     </li>
+//     <li class="nav-item">
+//       <a class="nav-link" href="#scrollspyHeading2">Second</a>
+//     </li>
+//     <li class="nav-item dropdown">
+//       <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
+//       <ul class="dropdown-menu">
+//         <li><a class="dropdown-item" href="#scrollspyHeading3">Third</a></li>
+//         <li><a class="dropdown-item" href="#scrollspyHeading4">Fourth</a></li>
+//         <li><hr class="dropdown-divider"></li>
+//         <li><a class="dropdown-item" href="#scrollspyHeading5">Fifth</a></li>
+//       </ul>
+//     </li>
+//   </ul>
+// </nav>
+// <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" class="scrollspy-example" tabindex="0">
+//   <h4 id="scrollspyHeading1">First heading</h4>
+//   <p>...</p>
+//   <h4 id="scrollspyHeading2">Second heading</h4>
+//   <p>...</p>
+//   <h4 id="scrollspyHeading3">Third heading</h4>
+//   <p>.ddddd..</p>
+//   <h4 id="scrollspyHeading4">Fourth heading</h4>
+//   <p>...</p>
+//   <h4 id="scrollspyHeading5">Fifth heading</h4>
+//   <p>...</p>
+// </div>
+//     `;
+// }
+// document.getElementById('head-about').innerHTML = headSection;
+
+
+// var num1 = window.prompt("Enter to Namber");
+// var num2 = window.prompt("Enter to Namber");
+
+// function getAvg(num1, num2);
+
+// {
+//     var sum = num1 + num2;
+//     var result = sum / 2;
+
+//     return result
+//     console.log(result);
+
+// }
+
+
+// var res = getAvg(40, 10);
+
+
+// document.getElementById("head").innerHTML = 10 + res
+// document.getElementById("head").innerHTML
+var rol = window.prompt("welcome")
+switch (rol) {
+    case ("admin");
+    console.log("hello admin")
+
+    break
+    case (user);
+    window.alert("no mseeag")
+
+    break
+}
